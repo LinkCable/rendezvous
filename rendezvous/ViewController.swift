2dc456c274ad544bd3ba79890907de12a0dc1c4b
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2015-09-04 22:32:40 -0400

    Initial Commit

---
+++ rendezvous/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rendezvous
+//
+//  Created by Philippe Kimura-Thollander on 9/4/15.
+//  Copyright © 2015 Philippe Kimura-Thollander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
