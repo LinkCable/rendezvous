a4d1cba86767f7bb29f8d697e588ed61c8498f35
Varun <sayalvarun@gmail.com>
2015-09-05 03:14:54 -0400

    map shit

--- rendezvous/ViewController.swift
+++ rendezvous/ViewController.swift
@@ -7,6 +7,8 @@
 //  Varun was here too xD
 //
 
+import Parse
+import Bolts
 import UIKit
 import AudioToolbox
 import AVFoundation
@@ -14,8 +16,11 @@ import MapKit
 import CoreLocation
 
 
-class ViewController: UIViewController {
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate{
     @IBOutlet weak var VibrateButton: UIButton!
+    
     @IBOutlet weak var map: MKMapView!
     let locationManager = CLLocationManager()
     
@@ -36,7 +41,33 @@ class ViewController: UIViewController {
             locationManager.startUpdatingLocation()
         }
     }
-
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        var locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        
+        let center = CLLocationCoordinate2D(latitude: locValue.latitude, longitude: locValue.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.map.setRegion(region, animated: false)
+        let currentLocation = CLLocation()
+        
+        var locationLat = currentLocation.coordinate.latitude
+        var locationLong = currentLocation.coordinate.longitude
+        if locationManager.location != nil {
+            print("locations = \(locationLat) \(locationLong)\(currentLocation.coordinate.latitude)\(currentLocation.coordinate.longitude)")
+            print()
+        }
+        else{
+            print("locationManager.location is nil")
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError){
+        print("Error while updating location " + error.localizedDescription)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
