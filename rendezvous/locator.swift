56aba70cf326920a73896fbc6813f48b52f76c06
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2015-09-06 03:24:04 -0400

    update feet label

--- rendezvous/locator.swift
+++ rendezvous/locator.swift
@@ -14,15 +14,30 @@ class locator: UIViewController {
     //RSSI[dbm] = −(10n log10(d) − A)
     
     var rssi: Int = 0
-    var meters: Float = 0
+    var meters: Double = 0
+    var SIGPOWER = -57 //constant rssi @ 1 meter
+    @IBOutlet weak var distance: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        rssiToMeters()
+        dispatch_async(dispatch_get_main_queue(), {
+            self.distance.text = String(self.meters * 3) + " feet away!"
+        })
     }
 
     func rssiToMeters() {
         if self.rssi > 0 {
             self.meters = 0
+        }
+        let ratio: Double = Double(Float(rssi)/Float(self.SIGPOWER));
+        
+        if (ratio < 1.0) {
+            self.meters = pow(ratio,10);
+        }
+        else {
+            self.meters =  (0.89976) * pow(ratio,7.7095) + 0.111;
         }
         
     }
