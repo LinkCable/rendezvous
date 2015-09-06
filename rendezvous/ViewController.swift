dad894012d30bf2962e7f10d07d6211c60faf69d
Varun <sayalvarun@gmail.com>
2015-09-05 20:56:53 -0400

    major pivot to bluetooth

--- rendezvous/ViewController.swift
+++ rendezvous/ViewController.swift
@@ -14,7 +14,7 @@ import AVFoundation
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate, FBSDKLoginButtonDelegate{
+class ViewController: UIViewController, CLLocationManagerDelegate{
     
     @IBOutlet weak var rSubtitle: UILabel!
     @IBOutlet weak var rTitle: UILabel!
@@ -23,6 +23,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, FBSDKLoginBut
     @IBOutlet weak var contacts: UIButton!
     
     let locationManager = CLLocationManager()
+    let appuid: String! = "29B1AD96-1DF0-4392-8C8A-7387F9E7BD84"
+    var region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "29B1AD96-1DF0-4392-8C8A-7387F9E7BD84")!, identifier: "Ya Boi")
     
     var uname: NSString = ""
     var id: NSString = ""
@@ -32,6 +34,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, FBSDKLoginBut
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print(appuid);
         
         rTitle.textAlignment = .Center
         rSubtitle.textAlignment = .Center
@@ -50,18 +54,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate, FBSDKLoginBut
         
         self.locationManager.requestWhenInUseAuthorization()
         
-        if(CLLocationManager.locationServicesEnabled()){
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-            locationManager.requestAlwaysAuthorization()
-            locationManager.startUpdatingLocation()
+        locationManager.delegate = self;
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse) {
+            locationManager.requestWhenInUseAuthorization()
         }
+        locationManager.startRangingBeaconsInRegion(region)
+        
         
         if (FBSDKAccessToken.currentAccessToken() != nil)
         {
             // User is already logged in, do work such as go to next view controller.
             getUserData()
         }
+    }
+    
+    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        print(beacons)
     }
     
     @IBAction func facebookLogin() {
@@ -74,35 +82,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate, FBSDKLoginBut
             }
             
             if (error == nil){
-                var fbLoginResult : FBSDKLoginManagerLoginResult = result
-                if(fbLoginResult.grantedPermissions.contains("email"))
+                var fbLoginResult : FBSDKLoginManagerLoginResult! = result
+                if(fbLoginResult != nil)
                 {
-                    self.getUserData()
+                    if(fbLoginResult.grantedPermissions.contains("email")){
+                        self.getUserData()
+                    }
                 }
             }
         })
     }
-    
-    /*func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        print("User Logged In")
-        
-        if ((error) != nil)
-        {
-            // Process error
-        }
-        else if result.isCancelled {
-            // Handle cancellations
-        }
-        else {
-            // If you ask for multiple permissions at once, you
-            // should check if specific permissions missing
-            if result.grantedPermissions.contains("email")
-            {
-                getUserData()
-                // Send allison data about my friends on Log in
-            }
-        }
-    }*/
     
     func getUserData()
     {
@@ -142,98 +131,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, FBSDKLoginBut
         }
     }
     
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
-        
-        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
-        
-        
-        
-        print("locations = \(locValue.latitude) \(locValue.longitude)")
-        
-        
-        
-        let center = CLLocationCoordinate2D(latitude: locValue.latitude, longitude: locValue.longitude)
-        
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        
-        
-        
-        //self.map.setRegion(region, animated: false)
-        
-        
-        
-        sendDataToServer("\(locValue.latitude)",long:"\(locValue.longitude)",id:"\(self.id)", uname:"\(self.uname)")
-        getDataFromServer()
-        
-    }
-    
-    func sendDataToServer(lat: String, long: String, id: NSString, uname: NSString){
-        
-        var request = NSMutableURLRequest(URL: NSURL(string: "http://104.131.188.22:3000/items")!, cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 5)
-        
-        var response: NSURLResponse?
-        
-        
-        
-        // create some JSON data and configure the request
-        
-        let jsonString = "json={\"lat\":\"\(lat)\",\"long\":\"\(long)\",\"id\":\"\(id)\",\"uname\":\"\(uname)\"}"
-        
-        request.HTTPBody = jsonString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
-        
-        request.HTTPMethod = "POST"
-        
-        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        
-        print(jsonString)
-        
-        
-        // send the request
-        
-        do{
-            
-            try NSURLConnection.sendSynchronousRequest(request, returningResponse: &response)
-            
-        }catch let error as NSError{
-            
-            print(error)
-            
-        }
-        
-        
-        
-        // look at the response
-        
-        //print("The response: \(response)")
-        
-        if let httpResponse = response as? NSHTTPURLResponse {
-            
-            //print("HTTP response: \(httpResponse.statusCode)")
-            
-        } else {
-            
-            print("No HTTP response")
-            
-        }
-        
-    }
-    
-    
-    func getDataFromServer(){
-        
-        let url = NSURL(string: "http://104.131.188.22:3000")
-        
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
-            
-            // Handle response here!
-            //print(NSString(data: data!, encoding: NSUTF8StringEncoding))
-            
-        }
-        
-        task.resume()
-        
-    }
-    
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError){
         print("Error while updating location " + error.localizedDescription)
     }
@@ -246,5 +143,96 @@ class ViewController: UIViewController, CLLocationManagerDelegate, FBSDKLoginBut
     @IBAction func buttonPressed(sender: AnyObject) {
         AudioServicesPlayAlertSound(SystemSoundID(4095))
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    func sendDataToServer(lat: String, long: String, id: NSString, uname: NSString){
+    
+    var request = NSMutableURLRequest(URL: NSURL(string: "http://104.131.188.22:3001/items")!, cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 5)
+    
+    var response: NSURLResponse?
+    
+    
+    
+    // create some JSON data and configure the request
+    
+    let jsonString = "json={\"lat\":\"\(lat)\",\"long\":\"\(long)\",\"id\":\"\(id)\",\"uname\":\"\(uname)\"}"
+    
+    request.HTTPBody = jsonString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+    
+    request.HTTPMethod = "POST"
+    
+    request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+    
+    print(jsonString)
+    
+    
+    // send the request
+    
+    do{
+    
+    try NSURLConnection.sendSynchronousRequest(request, returningResponse: &response)
+    
+    }catch let error as NSError{
+    
+    print(error)
+    
+    }
+    
+    
+    
+    // look at the response
+    
+    //print("The response: \(response)")
+    
+    if let httpResponse = response as? NSHTTPURLResponse {
+    
+    // print("HTTP response: \(httpResponse.statusCode)")
+    
+    } else {
+    
+    print("No HTTP response")
+    
+    }
+    
+    }
+    
+    
+    func getDataFromServer(){
+    
+    let url = NSURL(string: "http://104.131.188.22:3000")
+    
+    let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+    
+    // Handle response here!
+    //print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+    
+    }
+    
+    task.resume()
+    
+    }
+
+*/
 
 }
